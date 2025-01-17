b3284c9036e975d4645ba0de64181b7604e5715a
Samantha Bartlett <sammynilla@protonmail.com>
2025-01-17 09:30:45 -0900

    kill-bandcamp rename and update to 0.0.3

--- fire-monkey/kill_bandcamp.user.js
+++ fire-monkey/kill_bandcamp.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          Kill Bandcamp (View Embedded Player)
-// @version       0.0.2
+// @version       0.0.3
 // @description   Adds a button to view the album in an embedded player.
 // @author        sammynilla
 // @match         *://*.bandcamp.com/album/*
@@ -14,8 +14,12 @@
 
   // USER SETTINGS
   const BACKGROUND_COLOR = '#000000';
-  const LINK_COLOR = '#989898';
-  const PLAYER_SIZE = 'medium'; // 'small', 'medium' or 'large'
+  const LINK_COLOR       = '#989898';
+  const PLAYER_SIZE      = 'medium'; // 'small', 'medium' or 'large'
+  // (Recommended): Medium or Large for the best UI experience
+  // I suggest pairing this userscript with the following add-ons:
+  // - https://github.com/butterknight/bandcamp-volume-control
+  // - https://github.com/sammynilla/firefox/blob/main/fire-monkey/bandcamp_embedded_player.user.css
 
   // Start of UserScript
   let URL = document.head.querySelector("meta[property~='og:video']").content;
@@ -26,11 +30,22 @@
 
   // Default settings. Do not alter.
   props.set('tracklist', true);
-  PLAYER_SIZE.includes('small') ? 
-    props.delete('size') 
-  : props.set('size', PLAYER_SIZE);
+  props.set('size', PLAYER_SIZE);
+  switch (PLAYER_SIZE) {
+    case "tiny": {
+      return; // common tiny player L
+    }break;
+    case "small": {
+      props.delete('artwork'); // prevents small player from having a broken display
+    }break;
+    case "medium": {
+      // silly way to keep the player the medium size but still show the bandcamp volume control
+      props.set('tracklist', false);
+      props.set('size', 'large');
+    }break;
+  }
   // Integrate color props
-  props.set('bgcol', BACKGROUND_COLOR.replace('#', ''));
+  props.set('bgcol',   BACKGROUND_COLOR.replace('#', ''));
   props.set('linkcol', LINK_COLOR.replace('#', ''));
 
   // Prep the new URL
